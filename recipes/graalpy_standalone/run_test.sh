b4e5dbb2a02daa4702512872974d6f4d23719fde
Tim Felgentreff <timfel@DESKTOP-UOQCHD2>
2022-11-04 14:45:56 +0100

    another attempt at merging the graalpy recipes

--- recipes/graalpy_standalone/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -exo pipefail
-
-python3 --help
-python3 -c "import platform; print(platform._sys_version())"
-graalpy -m venv --help
-graalpy -m venv testvenv
-testvenv/bin/graalpy -c "import _struct; print(_struct.pack('I', 15663050))"
